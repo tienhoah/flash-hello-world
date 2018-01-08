36caad93beccc538d20709d5aff688bafa4a8acf
Hoa Hoang <nickhoang11@gmail.com>
2018-01-07 22:08:43 -0800

    Flask Exercise and error handling

--- app.py
+++ app.py
@@ -6,22 +6,30 @@ from flask import Flask
 #create the application object
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
 
 #use the decorator pattern to
 #link the view function to a url
 @app.route("/")
 @app.route("/hello")
-@app.route("/test")
-@app.route("/test/<search_query>")
 
 #define the view using a function, which returns a string
 def hello_world():
-	return "Hello, World!"
+	return "Hello, World! I love you Bi"
 
+#dynamic route
+@app.route("/test/<search_query>")
 def search(search_query):
 	return search_query
 
-
+#dynamic route with explicit status codes	
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael" :
+		return "Hello, {}".format(name)
+	else:
+		return "Not Found", 404
 
 #start the development server using the run() method
 if __name__ == "__main__":
